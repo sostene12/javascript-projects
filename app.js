3c60f5c6946f5d60ff18fb62351553e8a92785e1
sostene <ngarukiyimanasostene@gmail.com>
2022-05-08 10:00:50 +0200

    The popup box using javascript

--- app.js
+++ app.js
@@ -1,60 +1,22 @@
-//todo list project using javascript
-const list = document.querySelector(".todos");
-const addForm = document.querySelector(".add");
-const search = document.querySelector(".search input");
+// POPUP PROJECT USING JAVASCRIPT
 
-const generateTemplate = (todo) => {
-  const html = `
-  <li class="list-group-item d-flex justify-content-between align-items-center text-light">
-  <span>${todo}</span>
-  <i class="far fa-trash-alt delete"></i>
-</li>
-  `;
-  list.innerHTML += html;
-};
+/*
 
-// adding todos
-addForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const todo = addForm.add.value.trim();
-  if (todo.length) {
-    generateTemplate(todo);
-    addForm.reset();
-  }
+I'm a full stack developer using Mern-stack but a prefer Front-end ,i've gained skills through working of different projects 
+and internship at SOLVIT AFRICA and  i also use Javascript,PHP ,Bootstrap and Firebase. 
+I enjoy collaborating with team members as well as delivering the task given on time also i'm very passionate in codding
+
+*/
+const button = document.querySelector("button");
+const popup = document.querySelector(".popup-wrapper");
+const closeWrapper = document.querySelector(".popup-close");
+button.addEventListener("click", () => {
+  popup.style.display = "block";
+});
+closeWrapper.addEventListener("click", () => {
+  popup.style.display = "none";
 });
 
-// deleting todos
-list.addEventListener("click", (e) => {
-  if (e.target.classList.contains("delete")) {
-    e.target.parentElement.remove();
-  }
-});
-
-// search fuction
-const filterTodos = (term) => {
-  // add a class when  don't matches
-  Array.from(list.children)
-    .filter((todo) => {
-      return !todo.textContent.toLocaleLowerCase().includes(term);
-    })
-    .forEach((todo) => {
-      todo.classList.add("filtered");
-    });
-
-  // removing classname if they match
-  Array.from(list.children)
-    .filter((todo) => {
-      return todo.textContent.toLocaleLowerCase().includes(term);
-    })
-    .forEach((todo) => {
-      todo.classList.remove("filtered");
-    });
-
-  return true;
-};
-
-// Search by keyup event
-search.addEventListener("keyup", () => {
-  const term = search.value.trim().toLowerCase();
-  filterTodos(term);
+popup.addEventListener("click", () => {
+  popup.style.display = "none";
 });
