ddaf42ab06b6f19a02a0aa8ebbeb4a3a889cd8f7
sostene <ngarukiyimanasostene@gmail.com>
2022-05-08 09:54:19 +0200

    This is a todo list made by using vanilla javascript and bootstrap

--- app.js
+++ app.js
@@ -1,35 +1,60 @@
-// Ninja quiz project
-const correctAnswers = ["B", "B", "B", "B"];
+//todo list project using javascript
+const list = document.querySelector(".todos");
+const addForm = document.querySelector(".add");
+const search = document.querySelector(".search input");
 
-const form = document.querySelector(".quiz-form");
-const result = document.querySelector(".result");
+const generateTemplate = (todo) => {
+  const html = `
+  <li class="list-group-item d-flex justify-content-between align-items-center text-light">
+  <span>${todo}</span>
+  <i class="far fa-trash-alt delete"></i>
+</li>
+  `;
+  list.innerHTML += html;
+};
 
-form.addEventListener("submit", (e) => {
+// adding todos
+addForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  let score = 0;
-  const userAnswers = [
-    form.q1.value,
-    form.q2.value,
-    form.q3.value,
-    form.q4.value,
-  ];
-  //   check answers
-  userAnswers.forEach((answer, index) => {
-    if (answer === correctAnswers[index]) {
-      score += 25;
-    }
-  });
-  //   show the result on the page
-  scrollTo(0, 0);
-  result.querySelector("span").textContent = `${score}%`;
-  result.classList.remove("d-none");
-  let output = 0;
-  const timer = setInterval(() => {
-    result.querySelector("span").textContent = `${output}%`;
-    if (output === score) {
-      clearInterval(timer);
-    } else {
-      output++;
-    }
-  }, 20);
+  const todo = addForm.add.value.trim();
+  if (todo.length) {
+    generateTemplate(todo);
+    addForm.reset();
+  }
+});
+
+// deleting todos
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
+});
+
+// search fuction
+const filterTodos = (term) => {
+  // add a class when  don't matches
+  Array.from(list.children)
+    .filter((todo) => {
+      return !todo.textContent.toLocaleLowerCase().includes(term);
+    })
+    .forEach((todo) => {
+      todo.classList.add("filtered");
+    });
+
+  // removing classname if they match
+  Array.from(list.children)
+    .filter((todo) => {
+      return todo.textContent.toLocaleLowerCase().includes(term);
+    })
+    .forEach((todo) => {
+      todo.classList.remove("filtered");
+    });
+
+  return true;
+};
+
+// Search by keyup event
+search.addEventListener("keyup", () => {
+  const term = search.value.trim().toLowerCase();
+  filterTodos(term);
 });
