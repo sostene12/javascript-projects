871c7e96e6df94373fcdc68d7e08bb97c18b7048
sostene <ngarukiyimanasostene@gmail.com>
2022-05-08 12:15:35 +0200

    Digital clock using javascript

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
-// POPUP PROJECT USING JAVASCRIPT
+const clock = document.querySelector(".clock");
 
-/*
+const tick = () => {
+  const now = new Date();
+  const h = now.getHours();
+  const m = now.getMinutes();
+  const s = now.getSeconds();
+  const html = `
+    <span>${h}</span> :
+    <span>${m}</span> :
+    <span>${s}</span>
+  `;
+  clock.innerHTML = html;
+};
 
-I'm a full stack developer using Mern-stack but a prefer Front-end ,i've gained skills through working of different projects 
-and internship at SOLVIT AFRICA and  i also use Javascript,PHP ,Bootstrap and Firebase. 
-I enjoy collaborating with team members as well as delivering the task given on time also i'm very passionate in codding
-
-*/
-const button = document.querySelector("button");
-const popup = document.querySelector(".popup-wrapper");
-const closeWrapper = document.querySelector(".popup-close");
-button.addEventListener("click", () => {
-  popup.style.display = "block";
-});
-closeWrapper.addEventListener("click", () => {
-  popup.style.display = "none";
-});
-
-popup.addEventListener("click", () => {
-  popup.style.display = "none";
-});
+setInterval(tick, 1000);
