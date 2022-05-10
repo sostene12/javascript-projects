2a4d8bd7f8e3171aaf1cc8615ddeb17cc75311fd
sostene <ngarukiyimanasostene@gmail.com>
2022-05-10 11:12:28 +0200

    added icons folder and integrate with the api icon data to math with the UI and also completed the weather aapp

--- js/app.js
+++ js/app.js
@@ -16,6 +16,18 @@ const updateUI = ({ cityDetails, weather }) => {
   </div>
   `;
 
+  // update the night/day and icon images
+
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
+
+  let timeSrc = null;
+  if (weather.IsDayTime) {
+    timeSrc = "img/day.svg";
+  } else {
+    timeSrc = "img/night.svg";
+  }
+  time.setAttribute("src", timeSrc);
   // remove the d-none class if present
   if (card.classList.contains("d-none")) {
     card.classList.remove("d-none");
